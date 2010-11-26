f7f5d50bc1ec9e36befec9c729826c8ec3bc7642
Jay Luker <jluker@cfa.harvard.edu>
2010-11-26 14:26:50 -0500

    works by creating docset from ids and using that docset as a query filter

--- src/java/org/ads/solr/InvenioFacetComponent.java
+++ src/java/org/ads/solr/InvenioFacetComponent.java
@@ -6,8 +6,7 @@
 package org.ads.solr;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.search.Query;
 import org.apache.solr.common.SolrException;
@@ -21,6 +20,16 @@ import org.slf4j.LoggerFactory;
 import org.apache.commons.lang.StringUtils;
 import java.util.Random;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.FieldCache;
+import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.schema.SchemaField;
+import org.apache.solr.search.DocList;
+import org.apache.solr.search.DocListAndSet;
+import org.apache.solr.search.DocSet;
+import org.apache.solr.search.DocSlice;
+import org.apache.solr.search.SolrIndexSearcher;
 
 /**
  *
@@ -30,14 +39,13 @@ public class InvenioFacetComponent extends QueryComponent {
 
     public static final String COMPONENT_NAME = "invenio_facets";
 
-    @Override
+    public String[] docIds;
+
+    private HashMap<String, Integer> idMap = new HashMap<String, Integer>();
+
     public void prepare(ResponseBuilder rb) throws IOException {
 
         Logger log = LoggerFactory.getLogger(QueryComponent.class);
-
-        log.info("calling super class");
-        super.prepare(rb);
-        log.info("back in the child class now");
 
         SolrQueryRequest req = rb.req;
         SolrParams params = req.getParams();
@@ -45,41 +53,39 @@ public class InvenioFacetComponent extends QueryComponent {
             return;
         }
 
+        // let the main class do it's thing to set up the basic query
+        log.info("calling super class");
+        super.prepare(rb);
+        log.info("back in the child class now");
+
         // assume we've been passed in some kind of invenio intbitset and
         // parsed it into a set of doc ids use a canned integer list
         // log.info("Creating canned doc id array");
         //int[] docIds = {16939, 16025, 16021, 15964, 14850, 14763, 14762, 14482, 14481, 14452, 13841, 13838, 13825, 13822, 13815, 13776, 13773, 13768, 13756, 13744, 13742, 13734, 13428, 13345, 13344, 12862, 12853, 12833, 12826, 12562, 12560, 12534, 12530, 12522, 12480, 12479, 12478, 12467, 12223, 12221, 12084, 11803, 11785, 11762, 11760, 11757, 11753, 11566, 11148, 11147, 11095, 10844, 10840, 10839, 10783, 10769, 10768, 10760, 10748, 23313, 23308, 22752, 22741, 21922, 21916, 21396, 21356, 21239, 21236, 19163, 19118, 17953, 17949, 28123, 28121, 27289, 27288, 26582, 25919, 25837, 25825, 25656, 25128, 25062, 24927, 24926, 24160, 24158, 24153, 24148, 24058, 24055, 24054, 31606, 31598, 31502, 31277, 30948, 30901, 29911, 29878, 29875, 29874, 29873, 29832, 29681, 29679, 29678, 29600, 29596, 28981, 28980, 33384, 33272, 33271, 32800, 32798, 32794, 32562, 32559, 32555, 32509, 32443, 32441, 32386, 32338};
         //ArrayList<String> ids = new ArrayList<String>();
-//        String[] docIds = {"16939", "16025", "16021", "15964", "14850", "14763", "14762", "14482", "14481", "14452", "13841", "13838"};
-//        log.info("string id list: " + docIds);
-        String[] docIds = new String[6000000];
-        Random rgen = new Random();
-        for (int i = 0; i < 6000000; i++) {
-            docIds[i] = Integer.toString(rgen.nextInt(8900000));
-        }
+        this.docIds = new String[] {"16939", "16025", "16021", "15964", "14850", "14763", "14762", "14482", "14481", "14452", "13841", "13838"};
+        log.info("string id list: " + docIds);
+//        this.docIds = new String[6000000];
+//        Random rgen = new Random();
+//        for (int i = 0; i < 6000000; i++) {
+//            this.docIds[i] = Integer.toString(rgen.nextInt(8900000));
+//        }
 
-        String[] shortendDocIds = (String[])ArrayUtils.subarray(docIds, 0, 100);
-
-        try {
-            List<Query> filters = rb.getFilters();
-            if (filters==null) {
-                filters = new ArrayList<Query>();
-                rb.setFilters( filters );
-            }
-            String ids = String.format("id:(%s)", StringUtils.join(docIds, " "));
-            log.info("ids: " + StringUtils.substring(ids, 0, 100) + "...");
-            QParser id_parser = QParser.getParser(ids, null, req);
-            Query id_filter = id_parser.getQuery();
-            filters.add(id_filter);
-            log.info("done");
-        } catch (ParseException e) {
-            throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, e);
+        log.info("building idMap");
+        IndexReader reader = req.getSearcher().getReader();
+        String[] ids = FieldCache.DEFAULT.getStrings(reader, "id");
+        for (int i = 0; i < ids.length; i++) {
+            this.idMap.put(ids[i], i);
         }
+        log.info("this.idMap[47]: " + this.idMap.get("47"));
+        log.info("this.idMap[105]: " + this.idMap.get("105"));
+        log.info("this.idMap[10522]: " + this.idMap.get("10522"));
     }
 
-    /*
     @Override
     public void process(ResponseBuilder rb) throws IOException {
+
+        Logger log = LoggerFactory.getLogger(QueryComponent.class);
 
         SolrQueryRequest req = rb.req;
         SolrQueryResponse rsp = rb.rsp;
@@ -87,46 +93,45 @@ public class InvenioFacetComponent extends QueryComponent {
         if (!params.getBool(COMPONENT_NAME, true)) {
             return;
         }
+
+        long timeAllowed = (long)params.getInt( CommonParams.TIME_ALLOWED, -1 );
         SolrIndexSearcher searcher = req.getSearcher();
 
-        int[] luceneIds = new int[this.docIds.length];
         SchemaField idField = req.getSchema().getUniqueKeyField();
         log.info(idField.getName());
 
-        int docs = 0;
-        for (int i=0; i<this.docIds.length; i++) {
+        SolrIndexSearcher.QueryCommand cmd = rb.getQueryCommand();
+        cmd.setTimeAllowed(timeAllowed);
 
-            String docId = Integer.toString(this.docIds[i]);
-            int id = req.getSearcher().getFirstMatch(
-                new Term(idField.getName(), idField.getType().toInternal(docId)));
-            if (id >= 0) {
-                luceneIds[docs++] = id;
-            }
-            log.info(String.format("docid: %s, luceneid: %d, docs: %d", docId, id, docs));
+        log.info("cmd: " + cmd);
+        log.info("query: " + cmd.getQuery());
+        log.info("filters: " + cmd.getFilterList());
+        log.info("offset: " + cmd.getOffset());
+        log.info("len: " + cmd.getLen());
+        log.info("needDocSet: " + cmd.isNeedDocSet());
+
+        // translate our doc ids into lucene ids
+        int[] luceneIds = new int[this.docIds.length];
+        for (int i = 0; i < this.docIds.length; i++) {
+            log.info("i: " + i);
+            log.info("this.docIds[i]: " + this.docIds[i]);
+            luceneIds[i] = this.idMap.get(this.docIds[i]);
+            log.info("luceneIds[i]: " + luceneIds[i]);
         }
 
-        DocListAndSet res = new DocListAndSet();
-        res.docList = new DocSlice(0, docs, luceneIds, null, docs, 0);
-        log.info("docSet: " + res.docList.size());
+        DocList docSetFilter = new DocSlice(0, luceneIds.length, luceneIds, null, luceneIds.length, 0);
 
-        if (rb.isNeedDocSet()) {
-            List<Query> queries = new ArrayList<Query>();
-            queries.add(rb.getQuery());
-            List<Query> filters = rb.getFilters();
-            if (filters != null) queries.addAll(filters);
-            res.docSet = searcher.getDocSet(queries);
-        }
-//        SolrIndexSearcher.QueryCommand cmd = rb.getQueryCommand();
-//        log.info("cmd: " + cmd);
-//        log.info("query: " + cmd.getQuery());
-//        log.info("filters: " + cmd.getFilterList());
-//        log.info("offset: " + cmd.getOffset());
-//        log.info("len: " + cmd.getLen());
-//        log.info("needDocSet: " + cmd.isNeedDocSet());
+        // use our set of doc ids as a filter
+        cmd.setFilter(docSetFilter);
 
-        return;
+        SolrIndexSearcher.QueryResult result = new SolrIndexSearcher.QueryResult();
+        searcher.search(result,cmd);
+        rb.setResult( result );
+
+        rsp.add("response",rb.getResults().docList);
+        rsp.getToLog().add("hits", rb.getResults().docList.matches());
+
+        doFieldSortValues(rb, searcher);
+        doPrefetch(rb);
     }
-     *
-     */
-
 }
