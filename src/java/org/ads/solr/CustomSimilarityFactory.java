4eaf7dbd132c640a5dc1c9b3d5bc4d646a2cb1fb
Jay Luker <jluker@cfa.harvard.edu>
2011-03-04 15:25:46 -0500

    pulling SSS config values from schema.xml

--- src/java/org/ads/solr/CustomSimilarityFactory.java
+++ src/java/org/ads/solr/CustomSimilarityFactory.java
@@ -5,11 +5,15 @@
 
 package org.ads.solr;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import org.apache.lucene.search.Similarity;
 import org.apache.solr.schema.SimilarityFactory;
 import org.apache.lucene.misc.SweetSpotSimilarity;
 import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
 import org.apache.solr.core.SolrResourceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,29 +22,26 @@ import org.slf4j.LoggerFactory;
  *
  * @author jluker
  */
+//public class CustomSimilarityFactory extends SimilarityFactory {
 public class CustomSimilarityFactory extends SimilarityFactory {
 
     public static final Logger log = LoggerFactory.getLogger(SolrResourceLoader.class);
 
     @Override
-    public void init(SolrParams params) {
-        super.init(params);
-        log.info("similarity factory params: " + this.params);
-        log.info("toNamedList: " + this.params.toNamedList());
-        log.info("toNamedList['body']: " + this.params.toNamedList().get("body"));
-    }
-
-    @Override
     public Similarity getSimilarity() {
         SweetSpotSimilarity sim = new SweetSpotSimilarity();
-        Iterator<String> itr = this.params.getParameterNamesIterator();
-        while (itr.hasNext()) {
-            // TODO: figure out how to do this
-            log.info("field: " + itr.next());
-        }
+
+        int max = this.params.getInt("max");
+        int min = this.params.getInt("min");
+        float steepness = this.params.getFloat("steepness");
+
+        log.info("max: " + max);
+        log.info("min: " + min);
+        log.info("steepness: " + steepness);
+
         // hardcoded field settings for now
-        sim.setLengthNormFactors("body", 500, 10000, (float) 0.5, true);
-        sim.setLengthNormFactors("body_syn", 500, 20000, (float) 0.5, true);
+        sim.setLengthNormFactors("body", min, max, steepness, true);
+        sim.setLengthNormFactors("body_syn", min, max, steepness, true);
         return sim;
     }
 
